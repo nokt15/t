5892117d1a050cfc7d83887b37ee098c46786b3b
nokt15 <164750169+nokt15@users.noreply.github.com>
2024-03-24 17:09:01 +0300

    Add files via upload

---
+++ cub.py
@@ -0,0 +1,13 @@
+def triangle(n):
+   for i in range(1, n + 1):
+       print(i * '*')
+
+
+def square(n):
+   for i in range(n):
+       print(n * '*')
+
+
+def rect(m, n):
+   for i in range(n):
+       print(m * '*')
